a8dc6a8244e501e67cc6774c7c52a2a25057a48d
ikaronauta <jaortiz0496@misena.edu.co>
2022-10-20 20:26:30 -0500

    update

--- app/components/root/root.route.js
+++ app/components/root/root.route.js
@@ -1,4 +1,9 @@
 const route = require("express").Router();
-const root = require("./root.controllers.js");
+const root = require("./root.controller.js");
 
 route.get("/", root.get);
+route.post("/", root.post);
+route.put("/", root.put);
+route.delete("/:id", root.delete);
+
+module.exports = route;
--- app/index.js
+++ app/index.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+global.succes = require("./helpers/reponses").succes;
+global.error = require("./helpers/reponses").error;
 
 app.use("/api/v1", require("./routes.js"));
 
